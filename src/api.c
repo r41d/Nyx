3b75789a9ae8ccbffb4f978dc87a25f73141e914
Lennart Buhl <git@hackmate.de>
2015-12-10 20:38:43 +0100

    nyx_ prefix

--- src/api.c
+++ src/api.c
@@ -1,8 +1,11 @@
-
-#ifndef __API_H__
-#define __API_H__
-
+#include <stdlib.h>
 #include <stdint.h>
+#include <netdb.h> // needed for socket(), getprotobyname() and [struct protoent]
+//These are included in netdb.h:
+//  <sys/socket.h>
+//  <netinet/in.h>
+#include "api.h"
+#include "tcp_manager.h"
 
 /*
 Beschreibung
@@ -12,7 +15,18 @@ Rückgabewert
   Verbindung identifiziert. Dieser Wert wird File Descriptor genannt.
   Im Fehlerfall wird -1 zurückgegeben.
 */
-int nyx_accept(uint16_t port, uint32_t ipaddress);
+int nyx_accept(uint16_t port, uint32_t ipaddress) {
+
+    // make new raw socket
+    struct protoent* protocol_tcp = (struct protoent*) getprotobyname("TCP");
+    int new_raw_fd = socket(AF_INET, SOCK_RAW, protocol_tcp->p_proto);
+    free(protocol_tcp);
+
+    // remember this particular connection in our TCP state
+    tcp_manager_register(new_raw_fd, ipaddress, port);
+
+    // initiate three-way handshake
+}
 
 /*
 Beschreibung
@@ -25,7 +39,9 @@ Rückgabewert
   Gegenseite weniger Bytes versendet hat als in den Puffer passen.
   Im Fehlerfall wird -1 zurückgegeben.
 */
-ssize_t nyx_read(int fd, void* buf, size_t count);
+ssize_t nyx_read(int fd, void* buf, size_t count) {
+
+}
 
 /*
 Beschreibung
@@ -35,7 +51,9 @@ Rückgabewert
   Ist der Aufruf erfolgreich, wird die Anzahl der gesendeten Bytes zurückgegeben.
   Im Fehlerfall wird -1 zurückgegeben.
 */
-ssize_t nyx_write(int fd, void* buf, size_t count);
+ssize_t nyx_write(int fd, void* buf, size_t count) {
+
+}
 
 /*
 Beschreibung
@@ -46,7 +64,6 @@ Beschreibung
 Rückgabewert
   Keiner.
 */
-void nyx_close(int fd);
+void nyx_close(int fd) {
 
-
-#endif
+}
