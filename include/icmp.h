6eb250aedefe1ebb3d4df496d05a12e6194dd406
Lennart Buhl <git@hackmate.de>
2015-12-11 11:34:54 +0100

    assembly of icmp packets

--- include/icmp.h
+++ include/icmp.h
@@ -6,15 +6,14 @@
 //enum Himmelsrichtung{NORD, OST=90, SUED=180, WEST=270};
 typedef enum {
     ECHO_REPLY = 0,
-    DESTINATION_UNREACHABLE = 3,
-    REDIRECT_MESSAGE = 5,
+    // DESTINATION_UNREACHABLE = 3,
+    // SOURCE_QUENCH = 4,
+    // REDIRECT_MESSAGE = 5,
     ECHO_REQUEST = 8,
-    ROUTER_ADVERTISEMENT = 9,
-    ROUTER_SOLICITATION = 10,
-    TIME_EXCEEDED = 11,
-    PARAMETER_PROBLEM_BAD_IP_HEADER = 12,
-    TIMESTAMP = 13,
-    TIMESTAMP_REPLY = 14,
+    // TIME_EXCEEDED = 11,
+    // PARAMETER_PROBLEM = 12,
+    // TIMESTAMP = 13,
+    // TIMESTAMP_REPLY = 14,
 } icmp_type_t;
 
 typedef struct {
@@ -23,11 +22,17 @@ typedef struct {
 	uint8_t code;
     uint16_t checksum;
 
+    uint16_t ident;
+    uint16_t seq_num;
+
+    char* payload;
+        size_t payload_len;
 
 } icmp_header_t;
 
 void serialize_icmp(char* buf, const icmp_header_t* header);
 void deserialize_icmp(icmp_header_t* header, const char* buf);
 void dump_icmp_header(icmp_header_t* header);
-uint16_t icmp_checksum(const char* buf);
-//icmp_header_t* assemble_icmp_header(...);
+uint16_t icmp_checksum(const icmp_header_t* head);
+icmp_header_t* assemble_echo_request();
+icmp_header_t* assemble_echo_reply();
--- src/icmp.c
+++ src/icmp.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
 #include "icmp.h"
 
 void serialize_icmp(char* buf, const icmp_header_t* header) {
@@ -12,11 +15,61 @@ void dump_icmp_header(icmp_header_t* header) {
 
 }
 
-uint16_t icmp_checksum(const char* buf) {
+uint16_t icmp_checksum(const icmp_header_t* head) {
+    uint32_t sum = 0;
 
-    return -1;
+    sum += head->type << 8 | head->code;
+    sum += htons(head->checksum);
+    sum += htons(head->ident);
+    sum += htons(head->seq_num);
+
+    // Payload (16 bit words at a time)
+    for (size_t i = 0; i < (head->payload_len >> 1); ++i) {
+        sum += *(uint16_t *) &head->payload[i << 1];
+    }
+
+    // Folding carry values
+    sum = (sum & 0xFFFF) + (sum >> 16);
+    sum = (sum & 0xFFFF) + (sum >> 16);
+
+    return htons((uint16_t) ~sum);
 }
 
-//icmp_header_t* assemble_icmp_header(...) {
-//
-//}
+icmp_header_t* assemble_echo_request() {
+    static uint16_t seq = 0;
+	seq += 1;
+
+    icmp_header_t* icmp = (icmp_header_t*) malloc(sizeof(icmp_header_t));
+    icmp->type = ECHO_REQUEST;
+    icmp->code = 0;
+
+    icmp->ident = 42;
+    icmp->seq_num = seq;
+
+    char* str = "lolroflxd";
+    icmp->payload_len = sizeof(str);
+    icmp->payload = (char*) malloc(icmp->payload_len);
+    memcpy(icmp->payload, str, icmp->payload_len);
+
+    icmp->checksum = 0;
+    icmp->checksum = icmp_checksum(icmp);
+
+    return icmp;
+}
+
+icmp_header_t* assemble_echo_reply(icmp_header_t* echo_req) {
+    icmp_header_t* icmp = (icmp_header_t*) malloc(sizeof(icmp_header_t));
+    icmp->type = ECHO_REPLY;
+    icmp->code = 0;
+
+    icmp->ident = echo_req->ident;
+    icmp->seq_num = echo_req->seq_num;
+
+    icmp->payload_len = echo_req->payload_len;
+    memcpy(icmp->payload, echo_req->payload, echo_req->payload_len);
+
+    icmp->checksum = 0;
+    icmp->checksum = icmp_checksum(icmp);
+
+    return icmp;
+}
